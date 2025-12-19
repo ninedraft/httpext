2740cfa8845c2b482d275c62601a3d3382724493
merlin <clamores.pro@gmail.com>
2025-12-19 09:19:58 +0200

    implement ServeBytes function to serve byte data with sync.Pool for efficient memory management

--- handlers.go
+++ handlers.go
@@ -1,10 +1,42 @@
 package httpext
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+	"sync"
+	"time"
+)
 
 // Error replies to the request with the specified error message and HTTP code.
 //
 //	Error(w, http.StatusNotFound) = http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 func Error(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
+}
+
+func ServeBytes(filename string, data []byte) http.HandlerFunc {
+	modTime := time.Now().UTC()
+
+	pool := &sync.Pool{}
+
+	getReader := func() *bytes.Reader {
+		reader, _ := pool.Get().(*bytes.Reader)
+		if reader == nil {
+			reader = &bytes.Reader{}
+		}
+		reader.Reset(data)
+
+		return reader
+	}
+
+	putReader := func(reader *bytes.Reader) {
+		pool.Put(reader)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		re := getReader()
+		defer putReader(re)
+
+		http.ServeContent(w, r, filename, modTime, re)
+	}
 }
